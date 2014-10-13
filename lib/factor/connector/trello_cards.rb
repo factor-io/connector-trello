71a0699b16e873bf0d4f772226a1041a11d5fa2f
jozw <jozwright@gmail.com>
2014-10-13 16:59:16 -0700

    find card spec failing, WIP

--- lib/factor/connector/trello_cards.rb
+++ lib/factor/connector/trello_cards.rb
@@ -1,5 +1,6 @@
 require 'factor-connector-api'
 require 'trello'
+require 'pry'
 
 Factor::Connector.service 'trello_cards' do
   action 'create_card' do |params|
@@ -32,6 +33,33 @@ Factor::Connector.service 'trello_cards' do
       card = Trello::Card.create(content)
     rescue
       'Failed to create card'
+    end
+
+    action_callback card
+  end
+  action 'find_card' do |params|
+
+    card_id = params['card_id']
+    api_key = params['api_key']
+    auth_token = params['auth_token']
+
+    fail 'Card identification is required' unless card_id
+
+    info 'Initializing connection to Trello'
+    begin
+      Trello.configure do |config|
+        config.developer_public_key = api_key
+        config.member_token = auth_token
+      end
+    rescue
+      fail 'Authentication invalid'
+    end
+
+    info 'Finding card'
+    begin
+      card = Trello::Card.find(card_id)
+    rescue
+      fail 'Failed to find specified card'
     end
 
     action_callback card
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 
 describe 'trello_cards' do
+  before do
+    Trello.configure do |config|
+      config.developer_public_key = ENV['TRELLO_API_KEY']
+      config.member_token = ENV['TRELLO_AUTH_TOKEN']
+    end
+    @board = Trello::Board.create(name: 'test')
+    @list = Trello::List.create(board_id: @board.id, name: '123')
+    @card = Trello::Card.create(list_id: @list.id, name: '1')
+  end
+
   it 'can create a card' do
 
     api_key = ENV['TRELLO_API_KEY']
@@ -18,6 +28,23 @@ describe 'trello_cards' do
     service_instance.test_action('create_card', params) do
       expect_info message: 'Initializing connection to Trello'
       expect_info message: 'Creating new card'
+      expect_return
+    end
+  end
+
+  it 'can find a card' do
+
+    api_key = ENV['TRELLO_API_KEY']
+    auth_token = ENV['TRELLO_AUTH_TOKEN']
+    card_id = @card.id
+
+    service_instance = service_instance('trello_cards')
+
+    params = { 'card_id' => card_id }
+
+    service_instance.test_action('find_card', params) do
+      expect_info message: 'Initializing connection to Trello'
+      expect_info message: 'Finding card'
       expect_return
     end
   end
